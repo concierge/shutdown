641643f0b097f5252c66119b60bf01c5375f145a
Matthew Knox <mrk45@uclive.ac.nz>
2017-03-27 14:48:54 +1300

    ES6 and ignore 'this' context

--- shutdown.js
+++ shutdown.js
@@ -1,8 +1,12 @@
-var shutdownResponses = [$$`Good Night`, $$`I don't blame you`, $$`There you are`, $$`Please.... No, Noooo!`];
+const shutdownResponses = [
+    $$`Good Night`,
+    $$`I don't blame you`,
+    $$`There you are`,
+    $$`Please.... No, Noooo!`
+];
 
-exports.run = function(api, event) {
-    var index = Math.floor(Math.random() * shutdownResponses.length);
-    api.sendMessage(shutdownResponses[index], event.thread_id);
-    this.shutdown();
+exports.run = (api, event) => {
+    api.sendMessage(api.random(shutdownResponses), event.thread_id);
+    exports.platform.shutdown();
     return true;
 };
